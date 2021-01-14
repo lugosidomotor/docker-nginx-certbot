9e15ee50db139d6266288358db12141375acce34
Dömötör D. Lugosi <lugosi.domotor@gmail.com>
2021-01-14 16:19:21 +0100

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(marhavicces.hu www.marhavicces.hu)
+domains=(cloudtroopers.hu www.cloudtroopers.hu)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="lugosi.domotor@gmail.com" # Adding a valid address is strongly recommended
